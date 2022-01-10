e39a42adea78daca0c81c8d0ce1f4c76cb967592
shubuzuo <shubuzuo@gmail.com>
2022-01-10 19:03:41 +0800

    button 发送成功

--- handles/buttons.ts
+++ handles/buttons.ts
@@ -3,7 +3,8 @@ import { client } from "../client";
 
 export const buttons = async (msg: WAWebJS.Message) => {
     if (msg.body === '!buttons') {
-        // let button = new Buttons('Button body', [{ body: 'bt1' }, { body: 'bt2' }, { body: 'bt3' }], 'title', 'footer');
-        // await client.sendMessage(msg.from, button);
+        // @ts-ignore
+        let button = new Buttons('Button body', [{ body: 'bt1' }, { body: 'bt2' }, { body: 'bt3' }], 'title', 'footer');
+        await client.sendMessage(msg.from, button);
     }
 }
